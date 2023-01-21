c455fd825587eee5805d896f1af90f0ed6612af5
Vaibhavi <porevaibhavi5@gmail.com>
2023-01-21 18:06:40 +0530

    :zap: chore: filter items acc to category

--- client/src/components/List/List.js
+++ client/src/components/List/List.js
@@ -6,11 +6,32 @@ import styles from './List.module.css';
 
 const List = ({newItem}) => {
 
+    const [selCategory, setSelCategory] = useState("all");
     const [items, setItems] = useState([]);
     
     useEffect(() => {
         getItems().then(data => setItems(data));
     }, [newItem]);
+
+    let allItems = {};
+    for(let eachItem of items) {
+        if(allItems[eachItem.category]) {
+            allItems[eachItem.category].push({
+                "itemName": eachItem.itemName,
+                "quantity": eachItem.quantity,
+                "expiryDate": eachItem.expiryDate
+            });
+        }
+        else {
+            allItems[eachItem.category] = new Array();
+            allItems[eachItem.category].push({
+                "itemName": eachItem.itemName,
+                "quantity": eachItem.quantity,
+                "expiryDate": eachItem.expiryDate
+            });
+        }
+    } //dairy: milk,cheese,butter  fruits:apples,oranges
+    
     return(
         <>
             <React.Fragment>
@@ -18,8 +39,11 @@ const List = ({newItem}) => {
                     <div className={styles.category}>
                         <h2>Category</h2>
                             <div>
+                                <p onClick={() => setSelCategory("all")}>All</p>
                                 {
-                                    items.map(eachItem => <p key={items.indexOf(eachItem)}>{eachItem.category}</p>)
+                                    Object.keys(allItems).map(key =>
+                                        <p key={key} onClick={() => setSelCategory(key)}>{key}</p>
+                                    )
                                 }
                             </div>
                     </div>
@@ -28,13 +52,31 @@ const List = ({newItem}) => {
                         <h2>List</h2>
                             <div>
                                 {
-                                    items.map(eachItem => 
-                                        <div key={items.indexOf(eachItem)}>
-                                            <p>{eachItem.itemName}</p>
-                                            <p>{eachItem.quantity}</p>
-                                            <p>{eachItem.expiryDate}</p>
-                                        </div>
-                                    )
+                                    Object.keys(allItems).map(key => {
+                                        return(
+                                            selCategory === "all" ?
+                                            (
+                                                allItems[key].map(eachItem =>
+                                                    <div key={eachItem.itemName}>
+                                                        <p>{eachItem.itemName}</p>
+                                                        <p>{eachItem.quantity}</p>
+                                                        <p>{eachItem.expiryDate}</p>
+                                                    </div>
+                                                )
+                                                
+                                            ) :
+                                            (
+                                                key === selCategory && 
+                                                allItems[key].map(eachItem =>
+                                                    <div key={eachItem.itemName}>
+                                                        <p>{eachItem.itemName}</p>
+                                                        <p>{eachItem.quantity}</p>
+                                                        <p>{eachItem.expiryDate}</p>
+                                                    </div>
+                                                )
+                                            )
+                                        )
+                                    })
                                 }
                             </div>
                     </div>
