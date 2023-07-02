f8954535c326d1afcd00a33fafa971bc0e2b0470
Vaibhavi <porevaibhavi5@gmail.com>
2023-07-02 16:25:27 +0530

    add dependency vercel/analytics

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { inject } from '@vercel/analytics';
 
 import './index.css';
 import App from './App';
 
+inject();
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
