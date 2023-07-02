734ce1deead4fa8c06b4c560a3f876ae05543604
Vaibhavi <porevaibhavi5@gmail.com>
2023-07-02 19:13:42 +0530

    modify landing page

--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { Button } from "@mui/material";
+
+import styles from "./LandingPage.module.css";
 
 const LandingPage = () => {
-    return(
-        <>
-            <h1>Landing Page</h1>
-        </>
+    return (
+      <>
+        <div className={styles.main}>
+            <div className={styles.landingContent}>
+                <p className={styles.heading}>Upload & Relax</p>
+                <p className={styles.description}>
+                    A tool to help you reduce food wastage, effortlessly
+                </p>
+                <Button>Get Started</Button>
+            </div>
+        </div>
+      </>
     );
 }
 
