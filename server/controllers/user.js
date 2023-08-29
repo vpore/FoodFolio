6db966d0852d4b63e3750d98d733388f8496bd23
Vaibhavi <porevaibhavi5@gmail.com>
2023-08-29 18:08:32 +0530

    improve security

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,7 +1,11 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
+import dotenv from "dotenv";
 
 import User from '../models/user.js';
+
+dotenv.config();
+const secret = process.env.secret;
 
 export const signin = async (req, res) => {
     const { email, password } = req.body;
@@ -13,7 +17,7 @@ export const signin = async (req, res) => {
         const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
         if(!isPasswordCorrect) return res.status(400).json({message: 'Incorrect Password'});
 
-        const token = jwt.sign({email: existingUser.email, id: existingUser._id}, 'test');
+        const token = jwt.sign({email: existingUser.email, id: existingUser._id}, secret);
 
         res.status(200).json({result: existingUser, token});
 
@@ -33,7 +37,7 @@ export const signup = async (req, res) => {
         const hashedPassword = await bcrypt.hash(password, 12);
 
         const result = await User.create({email, password: hashedPassword, name: `${firstName} ${lastName}`});
-        const token = jwt.sign({email: result.email, id: result._id}, 'test', {expiresIn: '1h'});
+        const token = jwt.sign({email: result.email, id: result._id}, secret);
 
         res.status(200).json({result, token});
 
