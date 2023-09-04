92990316fb33d9e371705ce84ba0654093e62027
Vaibhavi <porevaibhavi5@gmail.com>
2023-09-04 18:31:51 +0530

    test email notif - 4

--- server/api/SendEmail.js
+++ server/api/SendEmail.js
@@ -5,14 +5,15 @@ import Notification from "../models/notification.js";
 import dotenv from "dotenv";
 dotenv.config();
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  auth: {
-    user: process.env.EMAIL_ID,
-    pass: process.env.EMAIL_PW,
-  },
-});
+  let transporter = nodemailer.createTransport({
+    host: "smtp-relay.brevo.com",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: process.env.EMAIL_ID, 
+      pass: process.env.EMAIL_PW, 
+    },
+  });
 
 // send email
 
